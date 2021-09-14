057c7a40f8a7f14070dcaca80ba4c9b9b062a239
Mark Otway <mark@otway.com>
2021-09-14 23:53:54 +0100

    Initial commit

---
+++ EFCore6JoinRepro/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Dotnet6Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore6JoinRepro
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static int basketEntryCount = 15;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("About to create SQLite DB...");
+
+                using var db = new TestContext();
+
+                if (db.BasketEntries.Count() != basketEntryCount)
+                {
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+
+                    Console.WriteLine("Successfully created DB...");
+
+                    GenerateTestData(500000);
+                }
+
+                RunTestQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception creating DB: {ex.Message}");
+                throw;
+            }
+        }
+
+        private static void RunTestQuery()
+        {
+            // Now the test query
+            using var db = new TestContext();
+
+            Console.WriteLine("Running query with bug...");
+
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var images = db.BasketEntries
+                           .Include(x => x.Image)
+                           .ThenInclude(x => x.ImageTags)
+                           .ThenInclude(x => x.Tag)
+                           .Select( x => x.Image )
+                           .ToArray();
+
+                Console.WriteLine($" Loaded {images.Count()} images, with {images.SelectMany(x => x.ImageTags).Count()} tags.");
+            }
+
+            watch.Stop();
+
+            Console.WriteLine($"Bug Query run 5x in {watch.ElapsedMilliseconds}ms ({watch.ElapsedMilliseconds/5}ms per run).");
+
+            watch.Reset();
+
+            Console.WriteLine("Running fixed query...");
+
+            watch.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var images = db.BasketEntries
+                     .Include(x => x.Image)
+                     .Select(x => x.Image)
+                     .ToArray();
+
+                foreach (var img in images )
+                {
+                    db.Entry(img)
+                        .Collection(e => e.ImageTags)
+                        .Query()
+                        .Include(e => e.Tag)
+                        .Load();
+                }
+
+                Console.WriteLine($" Loaded {images.Count()} images, with {images.SelectMany(x => x.ImageTags).Count()} tags.");
+            }
+
+            watch.Stop();
+
+            Console.WriteLine($"Fixed Query run 5x in {watch.ElapsedMilliseconds}ms ({watch.ElapsedMilliseconds / 5}ms per run).");
+
+        }
+
+        public static void GenerateTestData(int imageCount)
+        {
+            int tagCount = imageCount / 50;
+            int folderCount = imageCount / 20;
+            int basketCount = 5;
+
+            using var db = new TestContext();
+
+            Console.WriteLine($"Creating {folderCount} folders.");
+            for (int i = 0; i < folderCount; i++)
+            {
+                db.Add(new Folder());
+            }
+            db.SaveChanges();
+
+            Console.WriteLine($"Creating {imageCount} images + metadata.");
+            for (int i = 1; i <= imageCount; i++)
+            {
+                var img = new Image { FolderId = rnd.Next(1, folderCount) };
+                db.Add(img);
+                db.Add(new ImageMetaData { Image = img });
+            }
+            db.SaveChanges();
+
+            Console.WriteLine($"Creating {tagCount} Tags.");
+            for (int i = 0; i < tagCount; i++)
+            {
+                db.Add(new Tag());
+            }
+            db.SaveChanges();
+
+            Console.WriteLine($"Creating 3 ImageTags for every image.");
+            for (int imageId = 1; imageId <= imageCount; imageId++)
+            {
+                var randomTags = RandomUniqueNums(1, tagCount, 3);
+
+                randomTags.ForEach(x => db.Add(new ImageTag { ImageId = imageId, TagId = x }));
+
+                if (imageId % 10000 == 0)
+                {
+                    db.SaveChanges();
+                    Console.Write(".");
+                }
+            }
+
+            Console.WriteLine();
+            db.SaveChanges();
+
+            Console.WriteLine($"Creating {basketCount} Baskets.");
+            for (int i = 0; i < basketCount; i++)
+            {
+                db.Add(new Basket());
+            }
+            db.SaveChanges();
+
+            Console.WriteLine($"Creating {basketEntryCount} Basket Entries.");
+            for (int i = 0; i < basketEntryCount; i++)
+            {
+                int basketId = rnd.Next(1, basketCount);
+                int imageId = rnd.Next(1, imageCount);
+                db.Add(new BasketEntry { ImageId = imageId, BasketId = basketId });
+            }
+            db.SaveChanges();
+
+            Console.WriteLine("Test Data created.");
+        }
+
+        private static List<int> RandomUniqueNums(int min, int max, int count)
+        {
+            var results = new List<int>();
+
+            while (true)
+            {
+                int next = rnd.Next(min, max);
+                if (results.Contains(next))
+                    continue;
+
+                results.Add(next);
+
+                if (results.Count == count)
+                    break;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ EFCore6JoinRepro/TestModel.cs
@@ -0,0 +1,141 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Dotnet6Sqlite
+{
+    public class TestContext : DbContext
+    {
+        const string dbPath = "./TestDB.db";
+
+        public DbSet<Folder> Folders { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<ImageMetaData> ImageMetaData { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ImageTag> ImageTags { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketEntry> BasketEntries { get; set; }
+
+        /// <summary>
+        /// Basic initialisation for the DB that are generic to all DB types
+        /// </summary>
+        /// <param name="options"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            string dataSource = $"Data Source={dbPath}";
+            options.UseSqlite(dataSource);
+        }
+
+        /// <summary>
+        /// Called when the model is created by EF, this describes the key
+        /// relationships between the objects
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Many to many via ImageTags
+            var it = modelBuilder.Entity<ImageTag>();
+            it.HasKey(x => new { x.ImageId, x.TagId });
+
+            it.HasOne(p => p.Image)
+                .WithMany(p => p.ImageTags)
+                .HasForeignKey(p => p.ImageId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            it.HasOne(p => p.Tag)
+                .WithMany(p => p.ImageTags)
+                .HasForeignKey(p => p.TagId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<BasketEntry>()
+                .HasOne(a => a.Image)
+                .WithMany(b => b.BasketEntries);
+
+            modelBuilder.Entity<Image>()
+                .HasOne(img => img.MetaData)
+                .WithOne(meta => meta.Image)
+                .HasForeignKey<ImageMetaData>(i => i.ImageId);
+
+            modelBuilder.Entity<ImageTag>().HasIndex(x => new { x.ImageId, x.TagId }).IsUnique();
+            modelBuilder.Entity<Image>().HasIndex(x => new { x.FolderId });
+
+            modelBuilder.Entity<ImageMetaData>().HasIndex(x => x.ImageId);
+            modelBuilder.Entity<BasketEntry>().HasIndex(x => new { x.ImageId, x.BasketId }).IsUnique();
+        }
+    }
+
+    public class Folder
+    {
+        [Key]
+        public int FolderId { get; set; }
+    }
+
+    public class Image
+    {
+        [Key]
+        public int ImageId { get; set; }
+
+        public int FolderId { get; set; }
+        public virtual Folder Folder { get; set; }
+
+        public virtual ImageMetaData MetaData { get; set; }
+        // An image can appear in many baskets
+        public virtual List<BasketEntry> BasketEntries { get; } = new List<BasketEntry>();
+        // An image can have many tags
+        public virtual List<ImageTag> ImageTags { get; }
+    }
+
+    public class ImageMetaData
+    {
+        [Key]
+        public int MetaDataId { get; set; }
+
+        [Required]
+        public virtual Image Image { get; set; }
+        public int ImageId { get; set; }
+    }
+
+    public class Tag
+    {
+        [Key]
+        public int TagId { get; set; }
+
+        public virtual List<ImageTag> ImageTags { get; } = new List<ImageTag>();
+    }
+
+    public class ImageTag
+    {
+        [Key]
+        public int ImageId { get; set; }
+        public virtual Image Image { get; set; }
+
+        [Key]
+        public int TagId { get; set; }
+        public virtual Tag Tag { get; set; }
+
+    }
+
+    public class Basket
+    {
+        [Key]
+        public int BasketId { get; set; }
+
+        public virtual List<BasketEntry> BasketEntries { get; } = new List<BasketEntry>();
+    }
+
+    public class BasketEntry
+    {
+        [Key]
+        public int BasketEntryId { get; set; }
+
+        [Required]
+        public virtual Image Image { get; set; }
+        public int ImageId { get; set; }
+
+        [Required]
+        public virtual Basket Basket { get; set; }
+        public int BasketId { get; set; }
+    }
+}
